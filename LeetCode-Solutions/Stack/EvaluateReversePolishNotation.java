81161b5ca245cbbefa3f8f9b9fb4ec95354cadb5
burugusainitin <burugusainitin2027@gmail.com>
2025-12-15 10:13:09 +0530

    Add solution: Evaluate Reverse Polish Notation

--- LeetCode-Solutions/Stack/EvaluateReversePolishNotation.java
+++ LeetCode-Solutions/Stack/EvaluateReversePolishNotation.java
@@ -1,11 +1,10 @@
 import java.util.Stack;
 /**
- * Approach: Uses a Stack to evaluate the Reverse Polish Notation. The algorithm iterates over each token in the input array. If the token is an operator, it pops two numbers from the stack, applies the operation, and pushes the result back onto the stack. If the token is a number, it simply pushes the number onto the stack.
- * Time Complexity: O(n), where n is the number of tokens in the input array, since we process each token exactly once.
- * Space Complexity: O(n), where n is the number of tokens in the input array, since in the worst case, we might need to push all tokens onto the stack.
+ * Approach: Uses a Stack to evaluate the Reverse Polish Notation. It iterates over the tokens, pushing numbers onto the stack and popping the top two elements when an operator is encountered. The result of the operation is then pushed back onto the stack.
+ * Time Complexity: O(n), where n is the number of tokens. This is because we are doing a constant amount of work for each token.
+ * Space Complexity: O(n), where n is the number of tokens. This is because in the worst case, we might have to push all tokens onto the stack.
  */
-public class Solution {
-    class Solution {
+class Solution {
     public int evalRPN(String[] tokens) {
         String opr="+-*/";
         String nums="1234567890";
@@ -19,19 +18,16 @@ public class Solution {
                 case '+': stk.push(a+b);break;
                 case '-': stk.push(b-a);break;
                 case '*':stk.push(a*b);break;
-                case '/':
-                {System.out.println(a+" "+b+" "+b/a);
-                stk.push(b/a);break;}}
+                case '/':stk.push(b/a);break;
             }
-else stk.push(Integer.parseInt(tokens[i]));
-        }
-        System.out.println(stk);
-        return stk.pop();
+            }
+             else stk.push(Integer.parseInt(tokens[i]));
     }
-}
-}
+        return stk.pop();
 
-class Driver {
+    }}
+
+public class Driver {
     public static void main(String[] args) {
         Solution solution = new Solution();
         System.out.println(solution.evalRPN(new String[]{"2", "1", "+", "3", "*"})); // Expected output: 9
