e61bc58f53101461be2f20754d59b5b8d649f37a
burugusainitin <burugusainitin2027@gmail.com>
2025-12-20 13:18:22 +0530

    Add solution: Number of Substrings Containing All Three Characters

--- LeetCode-Solutions/StringManipulation/NumberOfSubstrings.java
+++ LeetCode-Solutions/StringManipulation/NumberOfSubstrings.java
@@ -1,9 +1,21 @@
+
 import java.util.HashMap;
 import java.util.Map;
+
 /**
- * Approach: Uses a Sliding Window technique with a HashMap to track the last occurrence of each character.
- * Time Complexity: O(n), where n is the length of the input string.
- * Space Complexity: O(1), as the HashMap stores a constant number of characters.
+ * This class provides a solution to the problem of finding the number of substrings 
+ * in a given string that contain all the characters 'a', 'b', and 'c' at least once.
+ * 
+ * Approach: 
+ * Uses a sliding window approach with a HashMap to track the indices of the characters 'a', 'b', and 'c'. 
+ * The algorithm iterates over the string, updating the indices in the HashMap and calculating the minimum index 
+ * to determine the number of substrings that contain all the required characters.
+ * 
+ * Time Complexity: 
+ * O(n), where n is the length of the input string, because each character in the string is visited once.
+ * 
+ * Space Complexity: 
+ * O(1), because the space used does not grow with the size of the input string, as the HashMap stores a constant number of characters.
  */
 class Solution {
     public int numberOfSubstrings(String s) {
@@ -20,9 +32,10 @@ class Solution {
         int min=Integer.MAX_VALUE;
             map.put(s.charAt(rt),rt);
             if(map.get('a')!=-1 && map.get('b')!=-1 && map.get('c')!=-1)
-            {for(Map.Entry<Character,Integer> entry:map.entrySet()){
-                min=Math.min(min,entry.getValue());
-            }
+            {min=Math.min(map.get('a'),Math.min(map.get('b'),map.get('c')));
+            // {for(Map.Entry<Character,Integer> entry:map.entrySet()){
+            //     min=Math.min(min,entry.getValue());
+            // }
             cnt+=1+min;
             }
             rt++;
@@ -31,11 +44,11 @@ class Solution {
     }
 }
 
-class Driver {
-    public static void main(String[] args) {
-        Solution solution = new Solution();
-        System.out.println(solution.numberOfSubstrings("abc"));
-        System.out.println(solution.numberOfSubstrings("aaaa"));
-        System.out.println(solution.numberOfSubstrings("abccba"));
-    }
+public class Driver {
+   public static void main(String[] args) {
+      Solution solution = new Solution();
+      System.out.println("Test Case 1: " + solution.numberOfSubstrings("abc"));
+      System.out.println("Test Case 2: " + solution.numberOfSubstrings("aaaaaa"));
+      System.out.println("Test Case 3: " + solution.numberOfSubstrings("abccba"));
+   }
 }
